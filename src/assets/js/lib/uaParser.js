bd6e286ad27871f6475257e3af98ea09f34742fc
Hayato Kamiyama <kami8ma8810@gmail.com>
2022-09-24 12:09:51 +0900

    snipet追加

--- src/assets/js/lib/uaParser.js
+++ src/assets/js/lib/uaParser.js
@@ -3,53 +3,53 @@
 const UAParser = require('ua-parser-js');
 
 const ua = (() => {
-  class UA {
-    constructor() {
-      this.uaParser = UAParser();
-      this.body = document.querySelector('body');
-    }
+	class UA {
+		constructor() {
+			this.uaParser = UAParser();
+			this.body = document.querySelector('body');
+		}
 
-    init() {
-      this.setUA();
-    }
+		init() {
+			this.setUA();
+		}
 
-    getBrowser() {
-      return this.uaParser.browser.name.replace(/\s+/g, '').toLowerCase();
-    }
+		getBrowser() {
+			return this.uaParser.browser.name.replace(/\s+/g, '').toLowerCase();
+		}
 
-    getDevice() {
-      const type = this.uaParser.device.type;
-      let type_result = '';
-      if (type == 'mobile') {
-        type_result = 'sp';
-      } else if (type == 'tablet') {
-        type_result = 'tb';
-      } else {
-        type_result = 'pc';
-      }
-      return type_result;
-    }
+		getDevice() {
+			const type = this.uaParser.device.type;
+			let type_result = '';
+			if (type == 'mobile') {
+				type_result = 'sp';
+			} else if (type == 'tablet') {
+				type_result = 'tb';
+			} else {
+				type_result = 'pc';
+			}
+			return type_result;
+		}
 
-    getModel() {
-      return this.uaParser.device.model
-        ? this.uaParser.device.model.replace(/\s+/g, '').toLowerCase()
-        : '';
-    }
+		getModel() {
+			return this.uaParser.device.model
+				? this.uaParser.device.model.replace(/\s+/g, '').toLowerCase()
+				: '';
+		}
 
-    getOS() {
-      return this.uaParser.os.name.replace(/\s+/g, '').toLowerCase();
-    }
+		getOS() {
+			return this.uaParser.os.name.replace(/\s+/g, '').toLowerCase();
+		}
 
-    setUA() {
-      this.body.classList.add(`ua-${this.getBrowser()}`);
-      this.body.classList.add(`ua-${this.getDevice()}`);
-      if (this.getModel()) {
-        this.body.classList.add(`ua-${this.getModel()}`);
-      }
-      this.body.classList.add(`ua-${this.getOS()}`);
-    }
-  }
-  return new UA();
+		setUA() {
+			this.body.classList.add(`ua-${this.getBrowser()}`);
+			this.body.classList.add(`ua-${this.getDevice()}`);
+			if (this.getModel()) {
+				this.body.classList.add(`ua-${this.getModel()}`);
+			}
+			this.body.classList.add(`ua-${this.getOS()}`);
+		}
+	}
+	return new UA();
 })();
 
 export { ua };
